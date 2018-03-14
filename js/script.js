83531b76725bf7a46695bf71386a937f61713246
Malmie <miechurska.m88@gmail.com>
2018-03-14 20:19:17 +0100

    implementing the model object

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*
 var randomLoc = Math.floor(Math.random() * 5);
 var location1 = randomLoc;
 var location2 = location1 + 1;
@@ -28,5 +29,37 @@ while (isSunk == false) {
 }
 var stats = "you took " + guesses + " guesses to sink the battleship, " + 
 			"which means your shooting accuracy was " + (3/guesses);
-alert(stats);
+alert(stats);*/
 
+var view = {
+	displayMessage: function(msg) {
+		var messageArea = document.getElementById("messageArea");
+		messageArea.innerHTML = msg;
+	},
+	displayHit: function(location) {
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "hit");
+	},
+	displayMiss: function(location) {
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "miss");
+	},
+};
+/*view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+view.displayMessage("Test");*/
+
+var model = {
+	boardSize: 7,
+	numShips: 3,
+	shipLength: 3,
+	shipsSunk: 0,
+
+ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+		{ locations: ["24", "34", "44"], hits: ["", "", ""] },
+		{ locations: ["10", "11", "12"], hits: ["", "", ""] }]
+};
