935cbd98274c337c7642d60fe5fabeda33ddfadc
Malmie <miechurska.m88@gmail.com>
2018-03-16 07:42:41 +0100

    implementing the controller

--- js/script.js
+++ js/script.js
@@ -45,13 +45,6 @@ var view = {
 		cell.setAttribute("class", "miss");
 	},
 };
-/*view.displayMiss("00");
-view.displayHit("34");
-view.displayMiss("55");
-view.displayHit("12");
-view.displayMiss("25");
-view.displayHit("26");
-view.displayMessage("Test");*/
 
 var model = {
 	boardSize: 7,
@@ -59,7 +52,55 @@ var model = {
 	shipLength: 3,
 	shipsSunk: 0,
 
-ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
-		{ locations: ["24", "34", "44"], hits: ["", "", ""] },
-		{ locations: ["10", "11", "12"], hits: ["", "", ""] }]
+	ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+			{ locations: ["24", "34", "44"], hits: ["", "", ""] },
+			{ locations: ["10", "11", "12"], hits: ["", "", ""] }],
+
+	fire: function(guess) {
+		for (var i=0; i<this.numShips; i++) {
+			var ship = this.ships[i];
+			var index = ship.locations.indexOf(guess);
+			if (index >= 0) {
+				ship.hits[index] = "hit";
+				view.displayHit(guess);
+				view.displayMessage("HIT!");
+				if (this.isSunk(ship)) {
+					view.displayMessage("You sank the ship!");
+					this.shipsSunk++;
+				}
+				return true;
+			}
+		}
+		view.displayMiss(guess);
+		view.displayMessage("You missed.");
+		return false;
+	},
+
+	isSunk: function(ship) {
+		for ( var i=0; i<this.shipLength; i++) {
+			if (ship.hits[i] !== "hit") {
+				return false;
+			}
+		}
+	}
 };
+
+function parseGuess(guess) {
+	var alphabet= ["A", "B", "C", "D", "E", "F", "G"];
+	if (guess === null || guess.length !== 2) {
+		alert("Please enter the letter and the number")
+	} else {
+		var firstChar = guess.charAt(0);
+		var row = alphabet.indexOf(firstChar);
+		var column = guess.charAt(1);
+		if (isNaN(row) || isNaN(column)) {
+			alert("Incorrect data");
+		} else if (row<0 || row>=model || column<0 || column>=model) {
+			alert("That is not within the board")
+		} else {
+			return row+column
+		}
+	}
+	return null;
+}
+console.log(parseGuess("B6"));
